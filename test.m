0f0a872ea52e3c75dbed1d3692ed31c06e28d6d4
Aaron <supermanaaronliu@gmail.com>
2019-04-23 11:30:42 +0800

    added an video

--- test.m
+++ test.m
@@ -1,14 +1,20 @@
+clear all;
+load('testingGT.mat')
+
 %% taking player photos, in this test, we are only using photo1
 [photo1,photo2] = takingPhoto();
-
+% photo1 = imread("photo1.jpg");
+% pause(0.5);
 %% get the initial position of the 5 tracked points
-I = im2double(rgb2gray(imread('test.jpg')));
+I = im2double(rgb2gray(imread('testing0.jpg')));
 % these are some test points on the palm of test.jpg
-p1 = [265,972];
-p2 = [436,883];
-p3 = [548,954];
-p4 = [433,1082];
-p5 = [471,1006];
+Xs = pts_x{1};
+Ys = pts_y{1};
+p1 = [Xs(1),Ys(1)];
+p2 = [Xs(2),Ys(2)];
+p3 = [Xs(3),Ys(3)];
+p4 = [Xs(4),Ys(4)];
+p5 = [Xs(5),Ys(5)];
 
 %% calculate the corrisponding point on the player photo
 [rp1,rp2,rp3,rp4,rp5] = rescaleImage(photo1, p1,p2,p3,p4,p5);
@@ -23,8 +29,29 @@ p5 = [471,1006];
 % 
 % maybe we could use this inv(H) to determine if a point is inside the
 % player photo (aka the paddle)
-H = projectPhoto(I, photo1, p1, p2,p3,p4,p5,rp1,rp2,rp3,rp4,rp5);
+v = VideoWriter('myFile.avi');
+v.FrameRate = 2;
+open(v);
+for i=0:15
+    I = im2double(rgb2gray(imread("testing"+i+".jpg")));
+    Xs = pts_x{i+1};
+    Ys = pts_y{i+1};
+    p1 = [Xs(1),Ys(1)];
+    p2 = [Xs(2),Ys(2)];
+    p3 = [Xs(3),Ys(3)];
+    p4 = [Xs(4),Ys(4)];
+    p5 = [Xs(5),Ys(5)];
+    
+    H = projectPhoto(I, photo1, p1, p2,p3,p4,p5,rp1,rp2,rp3,rp4,rp5);
+    
+    %% show the frame
+    B = projectPoint(H, photo1, I); % project each pixel on photo1 onto image I
+    
+    
+    imshow(B);
+    writeVideo(v,B);
+    pause(0.5);
+end
 
-%% show the frame
-B = projectPoint(H, photo1, I); % project each pixel on photo1 onto image I
-imshow(B);
+close(v);
+
